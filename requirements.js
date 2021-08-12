2614c577f550d22d2ab2753b0514f244cc64df03
AnoushkaSinha <85473755+AnoushkaSinha@users.noreply.github.com>
2021-08-12 10:45:47 +0530

    Add files via upload

---
+++ requirements.js
@@ -0,0 +1,49 @@
+//requirement for our project
+
+
+// we are book managemnet company
+
+
+//books
+//ISBN,TITLE,PUB DATE,language,num page ,author[], categories[]
+
+
+//authors
+//id,name,book[]
+
+
+//publications
+//id, name , books[]
+
+
+//we have to design and code an API over this.
+
+//1. BOOKS
+//we need an API :
+// to get all books
+//to get specific book
+//to get list of books based on category
+//to get list of books based on language
+
+
+//2.AUTHORS
+//we need an API
+//to get all authors
+//get specific author
+//to get list of AUTHORS BASED ON BOOKs
+
+
+//3. publications
+//API
+//all publications
+//specific publications
+//list of publications based on the books
+
+
+//post request
+//1.ADD NEW BOOK -done 
+//2.ADD NEW PUBLICATION-done
+//3.ADD NEW AUTHOR-done
+
+//put
+//update book details if author is changed.
