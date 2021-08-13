e3722ccdf9aa8943baf8f083076d3034f6338fbf
AnoushkaSinha <85473755+AnoushkaSinha@users.noreply.github.com>
2021-08-13 10:38:00 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,23 @@
+//MongoDB operators ->powerfull
+
+//Logical Operators
+
+//$inc -> increement, if we want to deecreement use  -1,-2,-3
+//$min => minimum
+//$max =>maximum
+//$set => used to set a data
+    //book.title="xyz"
+//$unset => removing a property from an object
+
+//Array Operators
+//$push
+//name=["anush","rhea"]
+//$pop
+//used to extract/remove/delete the last element
+//$pull
+// pull:{
+  //  name:"xyz"
+
+//}
+
+//$addToSet => doesnot allow duplicates
